9fd716fe10cbb0d9e1d3df86be0d336a49d3f13b
Lomig & Tux <lomigandtux@gmail.com>
2019-09-30 13:14:09 +0200

    hugo docker image change

--- hugo/hugo.alias.zsh
+++ hugo/hugo.alias.zsh
@@ -1,4 +1,4 @@
 # zsh aliases
-alias hugo-serve='docker run --rm -v $(pwd):/src -p 1313:1313 hugo server -D --bind=0.0.0.0'
-alias hugo='docker run --rm -v $(pwd):/src hugo'
+alias hugo-serve='docker run --rm -v $(pwd):/src -p 1313:1313 lomigandtux/hugo server -D --bind=0.0.0.0'
+alias hugo='docker run --rm -v $(pwd):/src lomigandtux/hugo'
 

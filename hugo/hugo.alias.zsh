d9583bdf6bc2a6c3e5e7e3546b852fd4faaf7a68
Lomig & Tux <lomigandtux@gmail.com>
2019-10-08 17:04:34 +0200

    Alpine version

--- hugo/hugo.alias.zsh
+++ hugo/hugo.alias.zsh
@@ -1,4 +1,4 @@
 # zsh aliases
-alias hugo-serve='docker run --rm -v $(pwd):/src -p 1313:1313 lomigandtux/hugo server -D --bind=0.0.0.0'
-alias hugo='docker run --rm -v $(pwd):/src lomigandtux/hugo'
+alias hugo-serve='docker run --rm -v $(pwd):/src -p 1313:1313 lomigandtux/hugo /usr/local/bin/hugo server -D --bind=0.0.0.0'
+alias hugo='docker run --rm -v $(pwd):/src lomigandtux/hugo /usr/local/bin/hugo'
 

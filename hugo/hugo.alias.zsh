3243682299c783a68558da8ac817f31fd0e5d639
Lomig & Tux <lomigandtux@gmail.com>
2019-10-12 00:47:36 +0200

    alias Hugo

--- hugo/hugo.alias.zsh
+++ hugo/hugo.alias.zsh
@@ -1,4 +1,6 @@
 # zsh aliases
-alias hugo-serve='docker run --rm -v $(pwd):/src -p 1313:1313 lomigandtux/hugo /usr/local/bin/hugo server -D --bind=0.0.0.0'
+alias hugo-edit='docker run --rm -v $(pwd):/src -p 1313:1313 lomigandtux/hugo /usr/local/bin/hugo server -D --bind=0.0.0.0'
+alias hugo-serve="docker run --rm -v /home/lomig/Documents/Blog/Hugo/:/src -v /home/lomig/Documents/Blog/Publication:/output -p 1313:1313 lomigandtux/hugo /usr/local/bin/hugo -d /output"
 alias hugo='docker run --rm -v $(pwd):/src lomigandtux/hugo /usr/local/bin/hugo'
+
 
